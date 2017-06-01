57065a461d52442830e888fc8d40078c8ed9b15e
Velichko Konstantin <zoxon.box@gmail.com>
2017-06-01 13:56:17 +0600

    Inital commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Monokai Light Theme
+
+VSCode Theme based on [Monokai Light](https://github.com/zoxon/monokai-light) theme
+
+## Screenshots
+### JavaScript
+![JavaScript](screenshots/js.png)
+
+### CSS
+![CSS](screenshots/css.png)
+
+### HTML
+![HTML](screenshots/html.png)
+
+### HTML
+![Ruby](screenshots/ruby.png)
+
+
+## Installing
+
+This extension is available for free in the [Visual Studio Code Marketplace](https://marketplace.visualstudio.com/)
+
+## What's new?
+
+Click here to go to the [Changelog](https://github.com/akamud/vscode-theme-onelight/blob/master/CHANGELOG.md)
+
+## Source code
+[Github](https://github.com/zoxon/vscode-theme-monokai-light)
+
+## Licence
+[MIT License](LICENSE.md)
---
+++ samples/JavaScript.js
@@ -0,0 +1,36 @@
+// Cross-browser xml parsing
+var parseXML = function( data ) {
+  var xml, tmp;
+  if ( !data || typeof data !== "string" ) {
+    return null;
+  }
+  try {
+    if ( window.DOMParser ) { // Standard
+      tmp = new DOMParser();
+      xml = tmp.parseFromString( data , "text/xml" );
+    } else { // IE
+      xml = new ActiveXObject( "Microsoft.XMLDOM" );
+      xml.async = false;
+      xml.loadXML( data );
+    }
+  } catch( e ) {
+    xml = undefined;
+  }
+  if ( !xml || !xml.documentElement || xml.getElementsByTagName( "parsererror" ).length ) {
+    jQuery.error( "Invalid XML: " + data );
+  }
+  return xml;
+};
+
+Sound.play = function() {}
+Sound.prototype = { something; }
+Sound.prototype.play = function() {}
+Sound.prototype.play = myfunc
+var parser = document.createElement('a');
+parser.href = "http://example.com:3000/pathname/?search=test#hash";
+parser.hostname; // => "example.com"
+
+
+
+
+
